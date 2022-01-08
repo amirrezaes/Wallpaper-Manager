8c6346e97ffe6a2d3e130e86c18b0e99f7056d9e
amirreza es <46327286+amirrezaes@users.noreply.github.com>
2022-01-07 17:29:54 -0800

    Update wallpaper_manager.py
    Added an infinite loop that will try to change the background and log any event

--- wallpaper_manager.py
+++ wallpaper_manager.py
@@ -3,7 +3,10 @@ import OperationSystems
 import api
 import random
 import json
+import time
+import logging
 
+logging.basicConfig(filename='event.log', encoding='utf-8', level=logging.DEBUG)
 
 config = json.load(open("config.json"))
 image_map = json.load(open("image_map.json"))
@@ -13,6 +16,7 @@ api_mode = None
 
 if config["image_folder_path"] != "default":
     OperationSystems.path = config["image_folder_path"]
+    logging.info(f"default path to images changed to: {OperationSystems.path}")
 
 if platform.system() == 'Windows':
     system = OperationSystems.Windows()
@@ -23,14 +27,26 @@ elif platform.system() == 'Darwin':
 else:
     system = OperationSystems.Operationsystem()
 
+logging.info(f"operation system is: {type(system).__name__}")
+
 if config["api_mode"] == "weather":
     api_mode = api.Weather
 
 elif config["api_mode"] == "time":
     api_mode = api.Time
 
+logging.info(f"api mode set to: {api_mode.__name__}")
 
-
-#a = random.choice(image_map[config["api_mode"]][api.Time.day_part()])
-b = random.choice(image_map[config["api_mode"]][api_mode.get_update()])
-system.change_bg(b)
+while True:
+    try:
+        api_resp = api_mode.get_update()
+        background = random.choice(image_map[config["api_mode"]][api_resp])
+        system.change_bg(background)
+        logging.info(f"api response: {api_resp}, background changed to {background}")
+        time.sleep(60*30) # half an hour
+    except KeyboardInterrupt:
+        logging.info("process interrupted")
+        exit()
+    except Exception as e:
+        logging.error(f"Error {e} occured while changing background")
+        exit()
